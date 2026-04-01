4de1f7c82e13791b758455284f629e0faae34fdc
Lawrence Chen <lawrencecchen@users.noreply.github.com>
2026-04-01 16:33:28 -0700

    Use socket env overrides in description UI tests

--- cmuxUITests/WorkspaceDescriptionUITests.swift
+++ cmuxUITests/WorkspaceDescriptionUITests.swift
@@ -97,12 +97,13 @@ final class WorkspaceDescriptionUITests: XCTestCase {
 
     private func configuredApp() -> XCUIApplication {
         let app = XCUIApplication()
-        app.launchArguments += ["-socketControlMode", "allowAll"]
         app.launchEnvironment["CMUX_UI_TEST_MODE"] = "1"
         app.launchEnvironment["CMUX_UI_TEST_GOTO_SPLIT_SETUP"] = "1"
         app.launchEnvironment["CMUX_UI_TEST_GOTO_SPLIT_PATH"] = dataPath
         app.launchEnvironment["CMUX_UI_TEST_FOCUS_SHORTCUTS"] = "1"
         app.launchEnvironment["CMUX_UI_TEST_SOCKET_SANITY"] = "1"
+        app.launchEnvironment["CMUX_SOCKET_ENABLE"] = "1"
+        app.launchEnvironment["CMUX_SOCKET_MODE"] = "allowAll"
         app.launchEnvironment["CMUX_SOCKET_PATH"] = socketPath
         app.launchEnvironment["CMUX_TAG"] = launchTag
         return app
